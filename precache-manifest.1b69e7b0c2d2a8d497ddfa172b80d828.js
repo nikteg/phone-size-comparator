9c45dfc6cb8c92c8af6940a92a2a07d1545b32ef
nikteg <nikteg@users.noreply.github.com>
2020-07-30 20:19:48 +0000

    deploy: ad7a08f7eced8474b8ad8855f057122121c539eb

---
+++ precache-manifest.1b69e7b0c2d2a8d497ddfa172b80d828.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e21cf4fa90b686502523aae72b405a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1176c6f6a6186abeb3b3",
+    "url": "/static/js/2.ad7d5bf9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ad7d5bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f36654af9592b36c69d8",
+    "url": "/static/js/main.da37b12e.chunk.js"
+  },
+  {
+    "revision": "27d528ef7d7b76daf1f0",
+    "url": "/static/js/runtime-main.2e9dcb0c.js"
+  }
+]);
