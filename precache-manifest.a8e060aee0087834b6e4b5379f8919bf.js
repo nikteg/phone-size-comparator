00f37c11c018323e6118cc03135fa7badeb10b00
nikteg <nikteg@users.noreply.github.com>
2020-07-30 20:29:01 +0000

    deploy: 69c2ae493c7280dd824cc3f1317a1d70e5f80a9c

---
+++ precache-manifest.a8e060aee0087834b6e4b5379f8919bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3313f0d6d0b127223bc9f385ab7d514d",
+    "url": "/phone-size-comparator/index.html"
+  },
+  {
+    "revision": "56dc66956c44f36388ab",
+    "url": "/phone-size-comparator/static/js/2.95b1ea3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/phone-size-comparator/static/js/2.95b1ea3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25609f65c81650f6f431",
+    "url": "/phone-size-comparator/static/js/main.a65c3852.chunk.js"
+  },
+  {
+    "revision": "d0b7fba541ea56bf407f",
+    "url": "/phone-size-comparator/static/js/runtime-main.411f232f.js"
+  }
+]);
